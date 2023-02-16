e0e4da79ef9ab89253c6ffa2f68af7006743b22e
hyeseong-dev <hyeseong43@gmail.com>
2023-02-16 14:32:32 +0900

    first mutation for InputType

---
+++ src/restaurants/dtos/create-restaurant.dto.ts
@@ -0,0 +1,13 @@
+import { Field, InputType } from '@nestjs/graphql'
+
+@InputType()
+export class createRestaurantDto {
+  @Field(type => String)  
+  name: string;
+  @Field(type => Boolean)  
+  isVegan: boolean;
+  @Field(type => String)  
+  address: string;
+  @Field(type => String)  
+  ownersName: string;
+}
--- src/restaurants/restaurants.resolver.ts
+++ src/restaurants/restaurants.resolver.ts
@@ -1,12 +1,17 @@
-import { Args, Query, Resolver } from '@nestjs/graphql';
+import { Args, Mutation, Query, Resolver } from '@nestjs/graphql';
+import { createRestaurantDto } from './dtos/create-restaurant.dto';
 import { Restaurant } from './entities/restaurant.entity';
 
 @Resolver(of => Restaurant)
 export class RestaurantResolver {
   @Query(returns => [Restaurant])
-  restaurants(@Args('veganOnly') veganonly: string): Restaurant[]{
-    console.log(veganonly);
-    
+  restaurants(@Args('veganOnly') veganonly: string): Restaurant[]{  
     return [];
+  }
+  @Mutation(returns => Boolean)
+  createRestaurant(@Args('createRestaurantInput') createRestaurantInput:createRestaurantDto) : boolean{
+    console.log(createRestaurantInput);
+    
+    return true;
   }
 }

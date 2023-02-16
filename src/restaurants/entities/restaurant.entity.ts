f7c5343e860a2a074d74d853ed76585105336ad5
hyeseong-dev <hyeseong43@gmail.com>
2023-02-16 11:56:35 +0900

    2.3 arguments

--- src/restaurants/entities/restaurant.entity.ts
+++ src/restaurants/entities/restaurant.entity.ts
@@ -4,6 +4,7 @@ import { Field, ObjectType } from '@nestjs/graphql';
 export class Restaurant {
   @Field(type => String)
   name: string;
+  
   @Field(type => Boolean, {nullable: true})
   isGood?: boolean;
 }
--- src/restaurants/restaurants.resolver.ts
+++ src/restaurants/restaurants.resolver.ts
@@ -1,10 +1,12 @@
-import { Query, Resolver } from '@nestjs/graphql';
+import { Args, Query, Resolver } from '@nestjs/graphql';
 import { Restaurant } from './entities/restaurant.entity';
 
-@Resolver()
+@Resolver(of => Restaurant)
 export class RestaurantResolver {
-  @Query(returns => Restaurant)
-  myRestaurant() {
-    return true
+  @Query(returns => [Restaurant])
+  restaurants(@Args('veganOnly') veganonly: string): Restaurant[]{
+    console.log(veganonly);
+    
+    return [];
   }
 }

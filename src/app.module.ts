70ad20f77eb5c39fb169f831811e960606944b4b
hyeseong-dev <hyeseong43@gmail.com>
2023-02-16 16:30:07 +0900

    Validating ConfigService

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,3 +1,4 @@
+import * as Joi from "joi";
 import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
@@ -11,6 +12,18 @@ import { RestaurantsModule } from './restaurants/restaurants.module';
       isGlobal: true,
       envFilePath: process.env.NODE_ENV === 'dev' ? '.env.dev' : '.env.test',
       ignoreEnvFile: process.env.NODE_ENV === 'prod',
+      validationSchema: Joi.object({
+        NODE_ENV: Joi.string()
+          .valid('dev', 'prod')
+          .required(),
+        DB_HOST: Joi.string().required(),
+        DB_PORT: Joi.string().required(),
+        DB_USERNAME: Joi.string().required(),
+        DB_PASSWORD: Joi.string().required(),
+        DB_NAME: Joi.string().required(),
+        DB_SYNCHRONIZE : Joi.string().required(),
+        DB_LOGGING: Joi.string().required(),
+      }),
     }),
     TypeOrmModule.forRoot({
       type: 'postgres',
@@ -19,8 +32,8 @@ import { RestaurantsModule } from './restaurants/restaurants.module';
       username: process.env.DB_USERNAME,
       password: process.env.DB_PASSWORD,
       database: process.env.DB_NAME,
-      synchronize: Boolean(process.env.SYNCHRONIZE),
-      logging: Boolean(process.env.LOGGING),
+      synchronize: Boolean(process.env.DB_SYNCHRONIZE),
+      logging: Boolean(process.env.DB_LOGGING),
     }),
     GraphQLModule.forRoot<ApolloDriverConfig>({
       driver: ApolloDriver,

e3c2103899496bc5988ab09e4e6cd425dcea60a5
hyeseong-dev <hyeseong43@gmail.com>
2023-02-16 10:28:14 +0900

    2.1 1st resolver

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,15 @@
+import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
 import { Module } from '@nestjs/common';
 import { GraphQLModule } from '@nestjs/graphql';
+import { RestaurantsModule } from './restaurants/restaurants.module';
 
 @Module({
   imports: [
-    GraphQLModule.forRoot()
+    GraphQLModule.forRoot<ApolloDriverConfig>({
+      driver: ApolloDriver,
+      autoSchemaFile: true,
+    }),
+    RestaurantsModule,
   ],
   controllers: [],
   providers: [],

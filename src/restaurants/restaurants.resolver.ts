9e4c1d498fb8905f2d4ad8a4270d44c7db0422e2
hyeseong-dev <hyeseong43@gmail.com>
2023-02-17 15:53:54 +0900

    4.8 Update Restaurant

--- src/restaurants/restaurants.resolver.ts
+++ src/restaurants/restaurants.resolver.ts
@@ -1,5 +1,6 @@
 import { Args, Mutation, Query, Resolver } from '@nestjs/graphql';
 import { createRestaurantDto } from './dtos/create-restaurant.dto';
+import { UpdateRestaurantDto } from './dtos/update-restaurant.dto';
 import { Restaurant } from './entities/restaurant.entity';
 import { RestaurantService } from './restaurants.service';
 
@@ -24,4 +25,19 @@ export class RestaurantResolver {
     }
     
   }
+
+  @Mutation(returns => Boolean)
+  async updateRestaurant(
+    @Args('inpu') updateRestaurantDto: UpdateRestaurantDto,
+  ): Promise<boolean> {
+    try {
+      await this.restaurantService.updateRestaurant(updateRestaurantDto);
+      return true;
+    } catch (e){
+      console.log(e);
+      return false;
+    }
+  }
+
+
 }
